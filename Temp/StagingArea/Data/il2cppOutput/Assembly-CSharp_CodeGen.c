788756059a75c3025b6c2f51d78d4f582d9f29ab
Erin Mittmann <mittmann.e@husky.neu.edu>
2021-01-07 16:18:57 -0500

    switched to url based

---
+++ Temp/StagingArea/Data/il2cppOutput/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,67 @@
+﻿#include "il2cpp-config.h"
+
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+IL2CPP_EXTERN_C_BEGIN
+IL2CPP_EXTERN_C_END
+
+
+
+
+// 0x00000001 System.Void CameraOrbit::Start()
+extern void CameraOrbit_Start_mC0E0151A9AABD7ED4F49837F6D9E29CD3256D03E (void);
+// 0x00000002 System.Void CameraOrbit::Update()
+extern void CameraOrbit_Update_mB75F5EE4B952AEE43BCBA045943E2ADAE59CB914 (void);
+// 0x00000003 System.Void CameraOrbit::.ctor()
+extern void CameraOrbit__ctor_m550725246CED41C4368A6D7A3CC7D8C1700848D5 (void);
+// 0x00000004 System.Void videoChange::ButtonForward()
+extern void videoChange_ButtonForward_m1DCB9E9A848C3EC7C0207453CF5ADDCC503FB5BF (void);
+// 0x00000005 System.Void videoChange::ButtonBackward()
+extern void videoChange_ButtonBackward_m1ACE91B921F22299C197F62B262733E739232890 (void);
+// 0x00000006 System.Void videoChange::.ctor()
+extern void videoChange__ctor_m814B2DEBFDD4880AD06EC3CA3330B056B7B6B788 (void);
+static Il2CppMethodPointer s_methodPointers[6] = 
+{
+	CameraOrbit_Start_mC0E0151A9AABD7ED4F49837F6D9E29CD3256D03E,
+	CameraOrbit_Update_mB75F5EE4B952AEE43BCBA045943E2ADAE59CB914,
+	CameraOrbit__ctor_m550725246CED41C4368A6D7A3CC7D8C1700848D5,
+	videoChange_ButtonForward_m1DCB9E9A848C3EC7C0207453CF5ADDCC503FB5BF,
+	videoChange_ButtonBackward_m1ACE91B921F22299C197F62B262733E739232890,
+	videoChange__ctor_m814B2DEBFDD4880AD06EC3CA3330B056B7B6B788,
+};
+static const int32_t s_InvokerIndices[6] = 
+{
+	23,
+	23,
+	23,
+	23,
+	23,
+	23,
+};
+extern const Il2CppCodeGenModule g_AssemblyU2DCSharpCodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharpCodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	6,
+	s_methodPointers,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+};
